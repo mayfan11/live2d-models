8ced3417abcbf10231171edc393cf69b39e858dd
yangyu <123456@qq.com>
2024-07-29 08:55:46 +0800

    1

--- scripts/build.js
+++ scripts/build.js
@@ -3,13 +3,15 @@ const path = require("path");
 const fs = require("fs");
 const package = require("../package.json");
 
+console.log(package.name);
 console.log(package.version);
 
 glob("models/**/*{model,model3}.json").then(function (contents) {
-  console.log(contents);
-  // "https://jsd.onmicrosoft.cn/npm/alligator-sinensis-live2d-model@1.0.1/models/haru/haru_greeter_t03.model3.json"
-  // fs.writeFile("data.json", JSON.stringify(contents, null, 2), (err) => {
-  //   if (err) throw err;
-  //   console.log("JSON data is written to data.json file.");
-  // });
+  const list = contents.map(item=>{
+    return `${package.name}@${package.version}/${item}`
+  })
+  fs.writeFile("dist/index.json", JSON.stringify(list, null, 2), (err) => {
+    if (err) throw err;
+    console.log("JSON data is written to data.json file.");
+  });
 });
