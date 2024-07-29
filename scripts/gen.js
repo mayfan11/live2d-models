0d887bbf6641207e6464a67a83884a52524ce61c
yangyu <123456@qq.com>
2024-07-29 17:27:13 +0800

    1

--- scripts/gen.js
+++ scripts/gen.js
@@ -10,12 +10,16 @@ glob("assets/**/*{model,model3,index}.json").then(function (contents) {
   const npmList = contents.map((item) => {
     return `${packageJson.name}@${packageJson.version}/${item}`;
   });
-  fs.writeFile("dist/models.json", JSON.stringify(contents, null, 2), (err) => {
-    if (err) throw err;
-    console.log("JSON data is written to data.json file.");
-  });
   fs.writeFile(
-    "dist/npm-models.json",
+    "menus/models.json",
+    JSON.stringify(contents, null, 2),
+    (err) => {
+      if (err) throw err;
+      console.log("JSON data is written to data.json file.");
+    }
+  );
+  fs.writeFile(
+    "menus/npm-models.json",
     JSON.stringify(npmList, null, 2),
     (err) => {
       if (err) throw err;
