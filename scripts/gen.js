dcaa8b383f5dc8807367bf60f83f59125cff3412
yangyu <123456@qq.com>
2024-07-30 09:24:21 +0800

    1

--- scripts/gen.js
+++ scripts/gen.js
@@ -3,44 +3,17 @@ import fs from "fs";
 import packageJson from "../package.json" assert { type: "json" };
 import { globby } from "globby";
 
-// glob("assets/**/*{model,model3,index}.json").then(function (contents) {
-//   console.log(contents.length);
-//   // const list = contents.map((item) => {
-//   //   return `${item}`;
-//   // });
-//   const npmList = contents.map((item) => {
-//     return `${packageJson.name}@${packageJson.version}/${item}`;
-//   });
-//   fs.writeFile(
-//     "menus/models.json",
-//     JSON.stringify(contents, null, 2),
-//     (err) => {
-//       if (err) throw err;
-//       console.log("JSON data is written to data.json file.");
-//     }
-//   );
-//   fs.writeFile(
-//     "menus/npm-models.json",
-//     JSON.stringify(npmList, null, 2),
-//     (err) => {
-//       if (err) throw err;
-//       console.log("JSON data is written to data.json file.");
-//     }
-//   );
-// });
-
 globby([
   "assets/**/*model.json",
   "assets/**/*model3.json",
   "assets/**/*index.json",
   "assets/**/*model*.json",
 ]).then((res) => {
-  console.log(res.length);
-  // const list = contents.map((item) => {
-  //   return `${item}`;
-  // });
   const npmList = res.map((item) => {
     return `${packageJson.name}@${packageJson.version}/${item}`;
+  });
+  const githubList = res.map((item) => {
+    return `${packageJson.author}/packageJson.name/${item}`;
   });
   fs.writeFile("menus/models.json", JSON.stringify(res, null, 2), (err) => {
     if (err) throw err;
@@ -49,6 +22,14 @@ globby([
   fs.writeFile(
     "menus/npm-models.json",
     JSON.stringify(npmList, null, 2),
+    (err) => {
+      if (err) throw err;
+      console.log("JSON data is written to data.json file.");
+    }
+  );
+  fs.writeFile(
+    "menus/github-models.json",
+    JSON.stringify(githubList, null, 2),
     (err) => {
       if (err) throw err;
       console.log("JSON data is written to data.json file.");
