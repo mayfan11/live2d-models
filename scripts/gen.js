a6f33ab3f1b969ede0d2532f56d8067e4e9feedf
yangyu <123456@qq.com>
2024-07-30 10:39:32 +0800

    1

--- scripts/gen.js
+++ scripts/gen.js
@@ -9,30 +9,31 @@ globby([
   "assets/**/*index.json",
   "assets/**/*model*.json",
 ]).then((res) => {
-  const npmList = res.map((item) => {
-    return `${packageJson.name}@${packageJson.version}/${item}`;
-  });
-  const githubList = res.map((item) => {
-    return `${packageJson.author}/${packageJson.name}/${item}`;
-  });
+  console.log(res.length);
+  // const npmList = res.map((item) => {
+  //   return `${packageJson.name}@${packageJson.version}/${item}`;
+  // });
+  // const githubList = res.map((item) => {
+  //   return `${packageJson.author}/${packageJson.name}/${item}`;
+  // });
   fs.writeFile("menus/models.json", JSON.stringify(res, null, 2), (err) => {
     if (err) throw err;
     console.log("JSON data is written to data.json file.");
   });
-  fs.writeFile(
-    "menus/npm-models.json",
-    JSON.stringify(npmList, null, 2),
-    (err) => {
-      if (err) throw err;
-      console.log("JSON data is written to data.json file.");
-    }
-  );
-  fs.writeFile(
-    "menus/github-models.json",
-    JSON.stringify(githubList, null, 2),
-    (err) => {
-      if (err) throw err;
-      console.log("JSON data is written to data.json file.");
-    }
-  );
+  // fs.writeFile(
+  //   "menus/npm-models.json",
+  //   JSON.stringify(npmList, null, 2),
+  //   (err) => {
+  //     if (err) throw err;
+  //     console.log("JSON data is written to data.json file.");
+  //   }
+  // );
+  // fs.writeFile(
+  //   "menus/github-models.json",
+  //   JSON.stringify(githubList, null, 2),
+  //   (err) => {
+  //     if (err) throw err;
+  //     console.log("JSON data is written to data.json file.");
+  //   }
+  // );
 });
