953105aae52ecf5881c8cf159edb1737ad888e7a
yangyu <123456@qq.com>
2024-07-30 08:55:42 +0800

    1

---
+++ scripts/gen copy.js
@@ -0,0 +1,29 @@
+import glob from "glob-promise";
+import fs from "fs";
+import packageJson from "../package.json" assert { type: "json" };
+
+glob("assets/**/*{model,model3,index}.json").then(function (contents) {
+  console.log(contents.length);
+  // const list = contents.map((item) => {
+  //   return `${item}`;
+  // });
+  const npmList = contents.map((item) => {
+    return `${packageJson.name}@${packageJson.version}/${item}`;
+  });
+  fs.writeFile(
+    "menus/models.json",
+    JSON.stringify(contents, null, 2),
+    (err) => {
+      if (err) throw err;
+      console.log("JSON data is written to data.json file.");
+    }
+  );
+  fs.writeFile(
+    "menus/npm-models.json",
+    JSON.stringify(npmList, null, 2),
+    (err) => {
+      if (err) throw err;
+      console.log("JSON data is written to data.json file.");
+    }
+  );
+});
