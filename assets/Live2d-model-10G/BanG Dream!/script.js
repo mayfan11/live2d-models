5acee6d5624a9baa8854da34df3830ff26e0b8fb
蓝鲸 <183675126@qq.com>
2024-08-04 11:20:35 +0800

    1

--- assets/Live2d-model-10G/BanG Dream!/script.js
+++ assets/Live2d-model-10G/BanG Dream!/script.js
@@ -1,12 +1,35 @@
 import { globby } from "globby";
+import path from "path";
+
+// globby([
+//   "**/*.moc",
+//   //   "**/.model.json",
+// ]).then((resList) => {
+//   console.log(resList.length);
+//   for (const mocItem of resList) {
+//     console.log(mocItem);
+//     // console.log(path.dirname(mocItem) + "/" + path.basename(mocItem));
+//     console.log(path.basename(mocItem));
+//     const obj = {
+//       model: path.basename(mocItem),
+//       textures: ["kasumi_2018dog_t04.1024/texture_00.png"],
+//     };
+//   }
+// });
+
+const mocItem = `asneeded/live2d/chara/027/027_live_event_111_ssr/Gt_event111.moc`;
+
+const dirname = path.dirname(mocItem); // startapp/live2d/chara/027/027_school_winter
+const basename = path.basename(mocItem); // Gt_school_winter_v3.moc
+
+console.log(dirname);
+console.log(basename);
 
 globby([
-  "assets/**/model.json",
-  "assets/**/model3.json",
-  "assets/**/*.model.json",
-  "assets/**/*.model3.json",
-  "assets/**/.model.json",
-  "assets/**/.model3.json",
+  `${dirname}/**/*.png`,
+  //   "**/*.png",
 ]).then((resList) => {
+  console.log(resList);
+  const resList = resList.map((item) => item.splite("/"));
   console.log(resList);
 });
