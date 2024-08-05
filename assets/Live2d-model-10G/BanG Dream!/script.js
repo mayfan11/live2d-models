0fc0649a73ab1987c3cb19b7e558dac62a3f8ca8
yangyu <123456@qq.com>
2024-08-05 14:01:43 +0800

    1

--- assets/Live2d-model-10G/BanG Dream!/script.js
+++ assets/Live2d-model-10G/BanG Dream!/script.js
@@ -2,14 +2,16 @@ import { globby } from "globby";
 import path from "path";
 import fs from "fs";
 
-globby(["**/*.moc"]).then(async (resList) => {
+globby(["**/*.moc *"]).then(async (resList) => {
   console.log(resList.length);
+  // console.log(resList);
+
   for (const mocItem of resList) {
     const dirname = path.dirname(mocItem);
-    // console.log(dirname);
-    // console.log(`${dirname}/model.json`);
-    const a = await globby([`${dirname}/model.json`]);
-    if (!a.length) {
+    //   // console.log(dirname);
+    //   // console.log(`${dirname}/model.json`);
+    const finds = await globby([`${dirname}/model.json`]);
+    if (!finds.length) {
       console.log(mocItem);
     }
   }
